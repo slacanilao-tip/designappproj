92d29023aeb9a0c8eba9731ff59a69fa39c69ace
slacanilao-tip <qsmlacanilao@tip.edu.ph>
2021-11-20 10:02:14 +0800

    Added files for designappproj

---
+++ tempdir/design_app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+
+sample = Flask(__name__)
+
+@sample.route("/")
+def main():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    sample.run(host="0.0.0.0", port=5050)
